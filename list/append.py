3691291fb89eaf18ae2ea22cb1f9af12464d68d5
charuzain <charuabhizain@gmail.com>
2022-08-08 21:54:06 -0400

    Appending to List

---
+++ list/append.py
@@ -0,0 +1,13 @@
+# Jiho works for a gardening store called Petal Power. Jiho keeps a record of orders in a list called orders.
+# orders = ["daisies", "periwinkle"]
+
+# * Use print to inspect the orders he has received today.
+# * Jiho just received a new order for "tulips". Use append to add this string to orders.
+# * Another order has come in! Use append to add "roses" to orders.
+# * Use print to inspect the orders Jiho has received today.
+
+orders = ["daisies", "periwinkle"]
+print(orders)
+orders.append("tulips")
+orders.append("roses")
+print(orders)
