4b2fee9d909b17981dac2cc3f2e5998ee1e10d9e
charuzain <charuabhizain@gmail.com>
2022-08-10 23:01:55 -0400

    for loop

---
+++ control flow/loop/for.py
@@ -0,0 +1,28 @@
+
+Run the code.
+
+We should get an IndentationError because the print(game) line is not indented.
+
+Checkpoint 2 Passed
+
+Stuck? Get a hint
+2.
+Indent (2 spaces or tab) line 6 so that we don’t get an IndentationError when you run the code.
+
+Run the code again!
+
+Checkpoint 3 Passed
+
+Stuck? Get a hint
+3.
+Write a for loop that prints each sport in the list sport_games.
+
+board_games = ["Settlers of Catan", "Carcassone", "Power Grid", "Agricola", "Scrabble"]
+
+sport_games = ["football", "hockey", "baseball", "cricket"]
+
+for game in board_games:
+ print(game)
+
+for sport in sport_games:
+  print(sport)
